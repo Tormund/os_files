94f742ad9961bf7b4a963f6d88f6c72c4176db76
tormund <tormund@zeoalliance.com>
2017-10-26 13:30:04 +0300

    extend filters, remove unsued code

--- file_dialog/private/unix_dialog.nim
+++ file_dialog/private/unix_dialog.nim
@@ -5,76 +5,56 @@ export dialog_types
 
 nim_init()
 
-let
-    dialogFileOpenDefaultButtons*: seq[DialogButtonInfo] = @[
-      (title: "Cancel", responseType: RESPONSE_CANCEL.int),
-      (title: "Open", responseType: RESPONSE_ACCEPT.int)
-    ]
-
-    dialogFileSaveDefaultButtons*: seq[DialogButtonInfo] = @[
-      (title: "Cancel", responseType: RESPONSE_CANCEL.int),
-      (title: "Save", responseType: RESPONSE_ACCEPT.int)
-    ]
-
-    dialogFolderCreateDefaultButtons*: seq[DialogButtonInfo] = @[
-      (title: "Cancel", responseType: RESPONSE_CANCEL.int),
-      (title: "Create", responseType: RESPONSE_ACCEPT.int)
-    ]
-
-    dialogFolderSelectDefaultButtons*: seq[DialogButtonInfo] = @[
-      (title: "Cancel", responseType: RESPONSE_CANCEL.int),
-      (title: "Select", responseType: RESPONSE_ACCEPT.int)
-    ]
-
 proc show*(di: DialogInfo): string =
-
     var action: TFileChooserAction
-    var defaultButtons: seq[DialogButtonInfo]
+
+    var buttons = newSeq[tuple[title:string, rType:int]](2)
+    buttons[0] = (title: "Cancel", rType: RESPONSE_CANCEL.int)
+    buttons[1] = (title: "Open", rType: RESPONSE_ACCEPT.int)
 
     case di.kind:
     of dkSelectFile:
         action = TFileChooserAction.FILE_CHOOSER_ACTION_OPEN
-        defaultButtons = dialogFileOpenDefaultButtons
     of dkSaveFile:
         action = TFileChooserAction.FILE_CHOOSER_ACTION_SAVE
-        defaultButtons = dialogFileSaveDefaultButtons
+        buttons[1].title = "Save"
     of dkSelectFolder:
         action = TFileChooserAction.FILE_CHOOSER_ACTION_SELECT_FOLDER
-        defaultButtons = dialogFolderSelectDefaultButtons
+        buttons[1].title = "Select"
     else:
         action = TFileChooserAction.FILE_CHOOSER_ACTION_CREATE_FOLDER
-        defaultButtons = dialogFolderCreateDefaultButtons
+        buttons[1].title = "Select"
 
     var dialog = file_chooser_dialog_new(di.title.cstring, nil, action, nil)
-
-    var buttons = di.buttons
-    if buttons.isNil:
-        buttons = defaultButtons
+    defer: dialog.destroy()
 
     for button in buttons:
-        discard dialog.add_button(button.title.cstring, button.responseType.cint)
+        discard dialog.add_button(button.title.cstring, button.rType.cint)
 
     if di.folder.len > 0:
         discard dialog.set_current_folder(di.folder.cstring)
 
-    if not di.filters.isNil:
-        var pfi = file_filter_new()
+    if not di.filters.isNil and di.filters.len > 0:
+        var filters = newSeq[PFileFilter]()
+        var all = file_filter_new()
+        all.set_name("All")
+        filters.add(all)
         for fi in di.filters:
-            pfi.add_pattern(fi.string)
-        dialog.set_filter(pfi)
+            var pfi = file_filter_new()
+            pfi.add_pattern(fi.ext.cstring)
+            all.add_pattern(fi.ext.cstring)
+            pfi.set_name(fi.name.cstring)
+            filters.add(pfi)
 
-    var res = dialog.run()
+        for fi in filters:
+            dialog.add_filter(fi)
 
-    case res:
-    of RESPONSE_ACCEPT, RESPONSE_YES, RESPONSE_APPLY:
+    if dialog.run() in [RESPONSE_ACCEPT, RESPONSE_YES, RESPONSE_APPLY]:
         let fileChooser = cast[PFileChooser](pointer(dialog))
         result = $fileChooser.get_filename()
         di.checkExtensionOnSave(result)
-    of RESPONSE_REJECT, RESPONSE_NO, RESPONSE_CANCEL, RESPONSE_CLOSE:
-        result = nil
     else:
         result = nil
 
-    dialog.destroy()
     while events_pending() > 0:
         discard main_iteration()
