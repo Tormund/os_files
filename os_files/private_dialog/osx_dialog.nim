16dab5a4ae67782cd5e4755565d0802470201015
Volodymyr Melnychuk <valdema_r@hotmail.com>
2019-08-16 11:25:28 +0300

    cpp fixes (#6)

--- os_files/private_dialog/osx_dialog.nim
+++ os_files/private_dialog/osx_dialog.nim
@@ -1,33 +1,21 @@
 import dialog_types
 export dialog_types
 
+import darwin / objc / runtime
+import darwin / foundation
+
 import strutils
 
 {.passL: "-framework Foundation".}
 {.passL: "-framework AppKit".}
+when defined(cpp):
+    {.passC: "-ObjC++".}
 
 type NSSavePanel {.importobjc: "NSSavePanel*", header: "<AppKit/AppKit.h>", incompleteStruct.} = object
 type NSOpenPanel {.importobjc: "NSOpenPanel*", header: "<AppKit/AppKit.h>", incompleteStruct.} = object
 
-{.pragma: objc, header: "<Foundation/Foundation.h>", nodecl.}
-type NSObject* {.objc, importobjc, final.} = ptr object {.inheritable.}
-type NSString* {.objc, importobjc, final.} = ptr object of NSObject
-type NSArrayAbstract* {.objc, importc: "NSArray", final.} = ptr object of NSObject
-type NSArray*[T] = ptr object of NSArrayAbstract
-type NSMutableArray*[T] = ptr object of NSArray[T]
-type NSMutableArrayAbstract* {.objc, importc: "NSMutableArray", final.} = ptr object of NSArrayAbstract
-
 proc newOpenPanel: NSOpenPanel {.importobjc: "NSOpenPanel openPanel", nodecl.}
 proc newSavePanel: NSSavePanel {.importobjc: "NSSavePanel savePanel", nodecl.}
-
-proc newNSMutableArrayAbstract*(capacity: int): NSMutableArrayAbstract {.importobjc: "NSMutableArray arrayWithCapacity", nodecl.}
-proc newNSMutableArray*[T](capacity: int): NSMutableArray[T] {.inline.} = cast[NSMutableArray[T]](newNSMutableArrayAbstract(capacity))
-
-proc addObject*(a: NSMutableArrayAbstract, o: NSObject) {.importobjc, nodecl.}
-template add*[T](a: NSMutableArray[T], v: T) = cast[NSMutableArrayAbstract](a).addObject(v)
-
-proc NSStringWithString*(n: cstring): NSString {.importobjc: "NSString stringWithUTF8String", nodecl.}
-converter toNSString*(s: string): NSString = NSStringWithString(s)
 
 proc showOpen(di: DialogInfo): string =
     var dialog = newOpenPanel()
@@ -36,7 +24,7 @@ proc showOpen(di: DialogInfo): string =
     let path: cstring = if di.folder.len == 0: "" else: di.folder
     var cres: cstring
 
-    var filters = newNSMutableArray[NSString](di.filters.len())
+    var filters = newMutableArray[NSString]()
     for f in di.filters:
         filters.add(toNSString(f.ext.replace("*.", "")))
 
@@ -51,12 +39,12 @@ proc showOpen(di: DialogInfo): string =
         }
 
         if ([`filters` count] > 0)
-            [`dialog` setAllowedFileTypes: `filters`];
+            [`dialog` setAllowedFileTypes: (NSArray<NSString *> *)`filters`];
 
         `dialog`.title = [NSString stringWithUTF8String: `ctitle`];
         [`dialog` setDirectoryURL:[NSURL fileURLWithPath: [NSString stringWithUTF8String: `path`]]];
         if ([`dialog` runModal] == NSOKButton && `dialog`.URLs.count > 0)
-            `cres` = [`dialog`.URLs objectAtIndex: 0].path.UTF8String;
+            `cres` = (char *)[`dialog`.URLs objectAtIndex: 0].path.UTF8String;
     """.}
 
     if not cres.isNil:
@@ -69,7 +57,7 @@ proc showSave(di: DialogInfo): string =
     let path: cstring = if di.folder.len == 0: "" else: di.folder
     var cres: cstring
 
-    var filters = newNSMutableArray[NSString](di.filters.len())
+    var filters = newMutableArray[NSString]()
     for f in di.filters:
         filters.add(toNSString(f.ext.replace("*.", "")))
 
@@ -77,14 +65,14 @@ proc showSave(di: DialogInfo): string =
         [`dialog` setCanChooseFiles:YES];
 
         if ([`filters` count] > 0)
-            [`dialog` setAllowedFileTypes: `filters`];
+            [`dialog` setAllowedFileTypes: (NSArray<NSString *> *)`filters`];
 
 
         `dialog`.canCreateDirectories = true;
         `dialog`.title = [NSString stringWithUTF8String: `ctitle`];
         [`dialog` setDirectoryURL:[NSURL fileURLWithPath: [NSString stringWithUTF8String: `path`]]];
         if ([`dialog` runModal] == NSOKButton)
-                `cres` = `dialog`.URL.path.UTF8String;
+                `cres` = (char *)`dialog`.URL.path.UTF8String;
     """.}
 
     if not cres.isNil:
