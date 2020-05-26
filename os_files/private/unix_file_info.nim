f91c78c354c14b6beb053d47bfcf4f8c2dd879fa
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2020-05-26 12:52:34 +0300

    Fix memory leaks, cache theme (#11)

--- os_files/private/unix_file_info.nim
+++ os_files/private/unix_file_info.nim
@@ -1,40 +1,49 @@
 import strutils
-import oldgtk3/[gtk, gio, gdk_pixbuf]
+import oldgtk3/[gtk, gio, gdk_pixbuf, gobject]
 import oldgtk3/gdk except string
 import oldgtk3/glib except byte
 
+var theme: IconTheme
+
 proc iconBitmapForFile*(path: string, width, heigth: int):seq[byte]=
+    discard gtk.initCheckWithArgv()
+
     let file = newFileForPath(path)
     var err: GError
     let fi = queryInfo(file, "*", GFileQueryInfoFlags.NONE, nil, err)
     if not err.isNil:
         return
 
-    var gIcon = fi.getIcon()
-    var strIcon = ($gIcon.toString()).split(" ")
+    let gIcon = fi.getIcon()
+    let strIcon = ($gIcon.toString()).split(" ")
+    fi.objectUnref()
 
-    let displ = display_open(nil)
-    let screen = get_default_screen(displ)
-    let theme = iconThemeGetForScreen(screen)
+    if theme.isNil:
+        let displ = display_open(nil)
+        let screen = get_default_screen(displ)
+        theme = iconThemeGetForScreen(screen)
+        if theme.isNil:
+            close(displ)
+            return
+
     var kind = ""
     for i in strIcon:
         if theme.hasIcon(i):
             kind = i
             break
-    err = nil
 
     let pixBuff = theme.loadIcon(kind, width.cint, cast[IconLookupFlags](0), err)
     if err.isNil:
         let w = pixBuff.getWidth().int
         let h = pixBuff.getHeight().int
-        let pixels = pixBuff.getPixels()
 
         if w + h > 0:
+            let pixels = pixBuff.getPixels()
             let bufSize = w * h * 4
             result = newSeq[byte](bufSize)
             copyMem(addr result[0], pixels, bufSize)
 
-    close(displ)
+        pixBuff.unref()
 
 import osproc
 proc openInDefaultApp*(path:string)=
